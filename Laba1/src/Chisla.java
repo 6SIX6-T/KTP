0c401e30094cf18751eacb9dfdb5ba99d1ba5afe
6SIX6-T <gwav@mail.ru>
2020-10-11 21:19:29 +0300

    Laba1

---
+++ Laba1/src/Chisla.java
@@ -0,0 +1,25 @@
+public class Chisla
+{
+        public static void main(String[] args)
+        {
+            for (int i = 2; i < 101; i++)
+            {
+                if (isPrime(i))
+                {
+                    System.out.println(i);
+                }
+            }
+        }
+    // Проверяем   является ли число  простым
+        public static boolean isPrime(int n)
+        {
+            for (int i = 2; i < n; i++)
+            {
+                if (n%i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+}
